b2a01988aed7c8ba6d610a8abf3279c2bcdb2961
Sibiya666 <sc2tvsibiya@gmail.com>
2017-01-21 19:34:18 +0300

    Основной скрипт написан.

--- js/stat.js
+++ js/stat.js
@@ -1,36 +1,40 @@
 'use strict';
 
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-var names = ['Саша', 'Васа', 'Рус', 'Вы'];
-var times = [14, 20, 30, 40];
-
-
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(120, 20, 420, 270);
-
-  /*ctx.fillStyle = '#ffffff';
-  ctx.fillRect(100, 10, 420, 270);*/
-
-  ctx.beginPath();
-  ctx.arc(100, 82.5, 20, 0, 12);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(230, 82.5, 30, 0, 12);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(260, 82.5, 20, 0, 12);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(190, 122.5, 30, 0, 12);
-  ctx.stroke();
-
-
-
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 50);
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Список результатов!', 120, 70);
+  var step = 0;
+  var bestTime = -1;
+  for (var i = 0; i < times.length; i++) {
+    if (i !== 0) {
+      step += 90;
+    }
+    var time = parseInt(times[i], 10);
+    if (time > bestTime) {
+      bestTime = time;
+    }
+    var MAX_HEIGHT = 150;
+    var heightColumn = MAX_HEIGHT / bestTime * time;
+    ctx.font = '16px PT Mono';
+    ctx.fillText(time, 120 + step, 240 - heightColumn);
+    var name = names[i];
+    var saturate = 0.7 * Math.random();
+    if (name === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(3, 4 , 255, ' + saturate + ')';
+    }
+    ctx.fillRect(120 + step, 250 - heightColumn, 40, heightColumn);
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText(name, 120 + step, 270);
+  }
 };
-
-renderStatistics(ctx, names, times);
